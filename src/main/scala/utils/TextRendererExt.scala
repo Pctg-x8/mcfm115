7a96c120f9b8cb3cc738c1af25a02a0e8d088b5c
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-06-14 11:45:44 +0900

    fix: implicit wrapper for TextRendererExt

---
+++ src/main/scala/utils/TextRendererExt.scala
@@ -0,0 +1,12 @@
+package com.cterm2.mcfm115.utils
+
+import net.minecraft.client.font.TextRenderer
+
+package object TextRendererExt {
+  implicit final class Wrapper(val renderer: TextRenderer) extends AnyVal {
+    final def drawStringCentric(text: String, x: Float, y: Float, color: Int) = {
+      val sw = renderer getStringWidth text
+      this.renderer.draw(text, x - sw / 2.0f, y, color)
+    }
+  }
+}
