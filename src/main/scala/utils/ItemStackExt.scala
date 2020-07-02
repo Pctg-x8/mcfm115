a2f46bc0852f3cbb417aad5bc4306765aa218a8f
S.Percentage <Syn.Tri.Naga@gmail.com>
2020-07-02 23:27:41 +0900

    fix: expansion methods test

--- src/main/scala/utils/ItemStackExt.scala
+++ src/main/scala/utils/ItemStackExt.scala
@@ -3,24 +3,12 @@ package jp.ct2.mcfm115.utils
 import net.minecraft.item.ItemStack
 
 package object ItemStackExt {
-  implicit final class ItemStackExtWrapper(private val stack: ItemStack) extends AnyVal {
-    /**
-      * otherをthisにマージ可能かどうか
-      * マージ可能なら、クラフト時にItemStack.incrementで増やしても問題ないことになる
-      *
-      * @param other マージ対象のItemStack
-      */
-    final def canBeMerged(other: ItemStack): Boolean = {
-      if (this.stack.isEmpty() || other.isEmpty()) return true
-      if (!(this.stack isItemEqualIgnoreDamage other)) return false
-      if (this.stack.getCount() + other.getCount() > this.stack.getMaxCount()) return false
+  def (s: ItemStack) canBeMerged (other: ItemStack): Boolean = {
+      if (s.isEmpty() || other.isEmpty()) return true
+      if (!(s isItemEqualIgnoreDamage other)) return false
+      if (s.getCount() + other.getCount() > s.getMaxCount()) return false
 
       true
-    }
-
-    /**
-      * 同値判定(アイテムの個数とItemインスタンスで判定する)
-      */
-    final def isEqual(other: ItemStack): Boolean = this.stack.getCount() == other.getCount() && this.stack.getItem() == other.getItem()
   }
+  def (s: ItemStack) isEqual (other: ItemStack): Boolean = s.getCount() == other.getCount() && s.getItem() == other.getItem()
 }
